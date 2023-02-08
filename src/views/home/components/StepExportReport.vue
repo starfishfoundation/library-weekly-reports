346283548129581a883c6a405a2b4fe7f14ddb48
Theo Diamantidis <diamaltho@gmail.com>
2023-02-08 23:19:23 +0200

    ui: improve ui in finish step

--- src/views/home/components/StepExportReport.vue
+++ src/views/home/components/StepExportReport.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
 import IconCheck from '~icons/material-symbols/check-circle-rounded'
 
-import { transpose, tableToHtml } from '~/utils/spreadsheet'
-import { getDateTime } from '~/utils/time'
+import { tableToHtml, transpose } from '~/utils/spreadsheet'
 </script>
 
 <template>
   <div class="items-center">
+    <h2 class="mb-4 text-xl font-bold">
+      Instructions
+    </h2>
+    <ol class="mb-8 list-decimal text-left">
+      <li>Open the weekly report spreadsheet.</li>
+      <li>Click the "Copy" button below.</li>
+      <li>Paste the data in the spreadsheet.</li>
+    </ol>
     <button
+      class="btn-secondary btn-lg btn"
       @click="copyExcel"
-      class="btn btn-lg btn-secondary">
+    >
       Copy
-      <IconCheck class="inline-block" v-if="copied" />
+      <div v-if="copyStatus === 'pending'" class="radial-progress ml-2 animate-spin" style="--size:1rem;--thickness:2px;--value:90;" />
+      <IconCheck v-if="copyStatus === 'done'" class="ml-2 inline-block" />
     </button>
   </div>
 </template>
@@ -21,24 +30,25 @@ export default {
   props: ['report'],
   data() {
     return {
-      copied: false,
-    }
-  },
-  methods: {
-    copyExcel() {
-      const blob = new Blob([this.reportAsExcel], { type: 'text/html' })
-      setTimeout(() => {
-        navigator.clipboard.write([
-          new ClipboardItem({ [blob.type]: blob })
-        ])
-        this.copied = true
-      }, 3000)
+      copyStatus: null,
     }
   },
   computed: {
     reportAsExcel() {
       return tableToHtml(transpose(this.report))
-    }
+    },
+  },
+  methods: {
+    copyExcel() {
+      this.copyStatus = 'pending'
+      const blob = new Blob([this.reportAsExcel], { type: 'text/html' })
+      setTimeout(() => {
+        navigator.clipboard.write([
+          new ClipboardItem({ [blob.type]: blob }),
+        ])
+        this.copyStatus = 'done'
+      }, 3000)
+    },
   },
 }
 </script>
