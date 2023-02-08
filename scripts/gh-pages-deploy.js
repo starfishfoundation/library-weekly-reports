d3881f8720c39c9ed81f715c4330571d6108e044
Theo Diamantidis <diamaltho@gmail.com>
2023-02-08 21:25:55 +0200

    refactor: fix gh-pages-deploy script to be esm-style

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const execa = require('execa')
+import fs from 'node:fs/promises'
+import { execa } from 'execa'
 
 ;(async () => {
   try {
@@ -7,7 +7,7 @@ const execa = require('execa')
     console.log('Building started...')
     await execa('npm', ['run', 'build', '--', '--base=/library-weekly-reports/'])
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync('dist') ? 'dist' : 'build'
+    const folderName = (await fs.exists('dist')) ? 'dist' : 'build'
     await execa('git', ['--work-tree', folderName, 'add', '--all'])
     await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages'])
     console.log('Pushing to gh-pages...')
